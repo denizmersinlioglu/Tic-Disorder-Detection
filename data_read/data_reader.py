0dcacc09aa57c13d1110e535d44fd26d82aefe72
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2019-03-12 22:25:26 +0300

    Env Fix
    - Pipfile.lock and pipfile fix
    - Environment reset
    - Data parsed in array [6]

--- data_read/data_reader.py
+++ data_read/data_reader.py
@@ -1,13 +1,13 @@
 import serial
 import serial.rfc2217
 import serial.tools.list_ports
+import re
 # import matplotlib.pyplot as plt
 
-serialport = serial.Serial(
-    port='/dev/cu.SLAB_USBtoUART', baudrate=9600)
+serialport = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=9600)
 
 while True:
     command = serialport.readline()
     str_command = str(command)
-    
-    print(command)
+    parameters = re.findall("\d+\.\d+", str_command)
+    print(parameters)
